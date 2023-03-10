d8379a2e13381ece7ea78a4ff5f017fb2e4b36d3
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-03-10 18:15:03 +0500

    finished game

--- app.js
+++ app.js
@@ -1,4 +1,61 @@
-const gemeBoard = document.querySelector("#gameboard");
+const gameBoard = document.querySelector("#gameboard");
 const infoDisplay = document.querySelector("#info");
 
 const startCells = ["", "", "", "", "", "", "", "", ""];
+
+let go = "circle"
+infoDisplay.textContentm = "Circle goes first"
+
+function createBoard() {
+    startCells.forEach((cell, index) => {
+        const cellElements = document.createElement('div')
+        cellElements.classList.add('square')
+        cellElements.id = index
+        cellElements.addEventListener('click', addGo)
+        gameBoard.append(cellElements)
+    })
+}
+
+createBoard()
+
+function addGo(e) {
+    const goDisplay = document.createElement('div')
+    goDisplay.classList.add(go)
+    e.target.append(goDisplay)
+    go = go === "circle" ? "cross" : "circle"
+    infoDisplay.textContent = "it is now " + go + "'s go."
+    e.target.removeEventListener("click", addGo)
+    checkScore()
+}
+
+function checkScore() {
+    const allSquares = document.querySelectorAll(".square")
+    console.log(allSquares);
+    const winningCombos = [
+        [0,1,2], [3,4,5], [6,7,8],
+        [0,3,6], [1,4,7], [2,5,8],
+        [0,4,8], [2,4,6]
+    ]
+
+    winningCombos.forEach(array => {
+        const circleWins = array.every(cell => allSquares[cell].firstChild?.classList.contains('circle'))
+
+        if (circleWins) {
+            infoDisplay.textContent = "Circle Wins"
+            allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
+            return
+        }
+
+
+    })
+
+    winningCombos.forEach(array => {
+        const crossWins = array.every(cell => allSquares[cell].firstChild?.classList.contains('cross'))
+
+        if (crossWins) {
+            infoDisplay.textContent = "Circle Wins"
+            allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
+            return
+        }
+    })
+}
